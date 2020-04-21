a5493e971409d4a73ac847bb2363022e0bdd5549
Krzysiek Herod <krzysiek.herod@gmail.com>
2020-04-20 19:47:56 -0500

    Refactor tests for readability

--- spec/forest_spec.rb
+++ spec/forest_spec.rb
@@ -26,56 +26,36 @@ class Forest
   end
 end
 
-FOREST_TESTS_DIRECTORY = "forest/test/"
-
 describe Forest do
   it "passes all the tests for the interpreter (call, block, data) module" do
-    directory = "#{FOREST_TESTS_DIRECTORY}forest/"
-    files = [
-      "assert.forest",
-      "join.forest"
-    ]
-    files.each do |file|
-      forest = Forest.new(
-        file: "#{directory}#{file}",
-        dependencies: Forest::MinimalDependencies.new
-      )
-      result = forest.run
-
-      expect(result).to eq({ result: true })
-    end
+    assert_result_true(
+      "forest/",
+      Forest::MinimalDependencies.new,
+      [
+        "assert.forest",
+        "join.forest"
+      ]
+    )
   end
 
   it "passes all the tests for the CGS (context, set, get) module" do
-    directory = "#{FOREST_TESTS_DIRECTORY}cgs/"
-    files = [
-      "set_followed_by_get_same_context.forest",
-      "get_uses_set_from_closer_context.forest"
-    ]
-    files.each do |file|
-      forest = Forest.new(
-        file: "#{directory}#{file}",
-        dependencies: Forest::DependenciesWithCGS.new
-      )
-      result = forest.run
-
-      expect(result).to eq({ result: true })
-    end
+    assert_result_true(
+      "cgs/",
+      Forest::DependenciesWithCGS.new,
+      [
+        "set_followed_by_get_same_context.forest",
+        "get_uses_set_from_closer_context.forest"
+      ]
+    )
   end
 
   it "passes all the tests for the LN (later, now) module" do
-    directory = "#{FOREST_TESTS_DIRECTORY}ln/"
-    files = [
-      "later_now.forest",
-    ]
-    files.each do |file|
-      forest = Forest.new(
-        file: "#{directory}#{file}",
-        dependencies: Forest::DependenciesWithLN.new
-      )
-      result = forest.run
-
-      expect(result).to eq({ result: true })
-    end
+    assert_result_true(
+      "ln/",
+      Forest::DependenciesWithLN.new,
+      [
+        "later_now.forest"
+      ]
+    )
   end
 end
---
+++ spec/helpers.rb
@@ -0,0 +1,16 @@
+module Helpers
+  FOREST_TESTS_DIRECTORY = "forest/test/"
+
+  def assert_result_true(directory, dependencies, files)
+    directory = "#{FOREST_TESTS_DIRECTORY}#{directory}/"
+    files.each do |file|
+      forest = Forest.new(
+        file: "#{directory}#{file}",
+        dependencies: dependencies
+      )
+      result = forest.run
+
+      expect(result).to eq({ result: true })
+    end
+  end
+end
